f9ebeb1ec1a6941e885183ceecfc48dcc56dee8a
Carlos Henrique <carloshpds@gmail.com>
2016-07-31 03:25:07 -0300

    Enables popup only on  'netflix.com'

--- chromeExtension/background.js
+++ chromeExtension/background.js
@@ -12,7 +12,7 @@ chrome.runtime.onInstalled.addListener(function() {
         // That fires when a page's URL contains a 'g' ...
         conditions: [
           new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { urlContains: 'netflix' },
+            pageUrl: { urlContains: 'netflix.com' },
           })
         ],
         // And shows the extension's page action.
