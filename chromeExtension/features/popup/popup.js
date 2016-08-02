0ead0f0f4b0bca75249e0a5b1ce8480a7003b851
Carlos Henrique <carloshpds@gmail.com>
2016-08-01 23:05:52 -0300

    new popup layout

--- chromeExtension/features/popup/popup.js
+++ chromeExtension/features/popup/popup.js
@@ -1,15 +1,15 @@
-window.addEventListener('load', init);
+// window.addEventListener('load', init);
 
-function init() {
+// function init() {
 
-    var actualLanguage = window.navigator.language;
+//     var actualLanguage = window.navigator.language;
 
-     var newLanguage = document.getElementById("change-language");
-     newLanguage.addEventListener('mouseup', changeLanguage);
+//      var newLanguage = document.getElementById("change-language");
+//      newLanguage.addEventListener('mouseup', changeLanguage);
 
-     function changeLanguage() {
-         actualLanguage = newLanguage.value;         
-    };
+//      function changeLanguage() {
+//          actualLanguage = newLanguage.value;
+//     };
 
-};
+// };
 
