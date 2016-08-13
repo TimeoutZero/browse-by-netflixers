88151bbecc26ae3440f43a3bcb6abbf2ccd78b1f
Carlos Henrique <carloshpds@gmail.com>
2016-08-13 20:04:06 -0300

    Fix render delay

--- reactComponent/src/component.entry.js
+++ reactComponent/src/component.entry.js
@@ -3,11 +3,49 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/Main.prod';
 
-// Render the main component into the dom
-let liContainer = document.createElement("LI");
-liContainer.classList.add('bb-nettflixers-main-netflix-container');
+/**
+ * Variables
+ */
+let liContainer    = document.createElement("LI");
+let netflixNavList = null;
 
-let navigationList = document.querySelector(".akira-header ul[role='navigation']");
-navigationList.insertBefore( liContainer, navigationList.firstChild );
+/**
+ * @description discovers NETFLIX's elements which will wrap the application
+ */
+let discoverContainers = () => {
+  netflixNavList = document.querySelector(".akira-header ul[role='navigation']");
+};
 
-ReactDOM.render(<App />, liContainer );
+/**
+ * @description Render the main component into the dom
+ */
+let renderApp = () => {
+  liContainer.classList.add('bb-nettflixers-main-netflix-container');
+  netflixNavList.insertBefore( liContainer, netflixNavList.firstChild );
+  ReactDOM.render(<App />, liContainer );
+};
+
+/**
+ * @description Timeout function used to rerender when it's wrapper is not available
+ */
+let renderTimeout = () => {
+  window.setTimeout(tryToRender, 500);
+};
+
+/**
+ * @description Try to render app and schedules a retry application's wrapper is not available
+ */
+let tryToRender = () => {
+  discoverContainers();
+  if(netflixNavList){
+    renderApp();
+  } else {
+    renderTimeout();
+  }
+};
+
+/**
+ * Initialize
+ */
+tryToRender()
+
