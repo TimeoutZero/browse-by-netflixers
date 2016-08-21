084f7b480201651556d38bbf46621fbb497aebf1
Carlos Henrique <carloshpds@gmail.com>
2016-08-21 13:53:28 -0300

    #11 DONE

--- reactComponent/src/components/Main.prod.js
+++ reactComponent/src/components/Main.prod.js
@@ -1,9 +1,12 @@
 
 import React from 'react';
-import {IntlProvider} from 'react-intl';
+import {IntlProvider, addLocaleData} from 'react-intl';
 
 import CategoryBrowse from './CategoryBrowse/CategoryBrowse';
 import I18n from './CategoryBrowse/i18n';
+
+import ptLocaleData from 'react-intl/locale-data/pt';
+addLocaleData(ptLocaleData);
 
 let messages = I18n.discoverMessagesByLocale();
 
